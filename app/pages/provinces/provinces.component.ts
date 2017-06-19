3bb98cdb433bb04eace00304460ba87f6ac0acc1
Luis Arturo Mora <LuisArturo@Luiss-MacBook-Pro.local>
2017-06-19 12:56:03 -0400

    Working on ios

--- app/pages/provinces/provinces.component.ts
+++ app/pages/provinces/provinces.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from "@angular/core";
 import { Router } from '@angular/router';
 import * as SocialShare from 'nativescript-social-share';
+import { Page } from 'ui/page';
 
 import { Province } from '../../models/provinces/provinces';
 import { Data } from '../../models/provinces/province.service';
@@ -8,17 +9,20 @@ import { Data } from '../../models/provinces/province.service';
 @Component({
   selector: "province",
   templateUrl: "pages/provinces/provinces.html",
-  styleUrls: ["pages/provinces/provinces.css"],
+  styleUrls: ["pages/provinces/provinces-common.css", "pages/provinces/provinces.css"],
 })
 
 export class ProvincesComponent implements OnInit {
   provincesList: Array<Province> = [];
   isLoading = false;
   listLoaded = false;
+  frame = {};
 
-  constructor(private router : Router, private data : Data){}
+  constructor(private router : Router, private data : Data, private page : Page){
+  }
 
   ngOnInit() {
+    this.page.actionBarHidden = false;
     this.isLoading = true;
     this.provincesList = this.data.provinces;
     this.isLoading = false;
