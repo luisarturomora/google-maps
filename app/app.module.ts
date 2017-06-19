1a8a88e1796048f901d7ced648124147e6efa845
Luis Arturo Mora <LuisArturo@Luiss-MacBook-Pro.local>
2017-06-19 09:28:14 -0400

    Third Commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,6 +7,13 @@ import { AppComponent } from "./app.component";
 import { routes, navigatableComponents } from "./app.routing";
 import { Data } from './models/provinces/province.service';
 
+import * as platform from "platform";
+declare var GMSServices: any;
+
+if (platform.isIOS) { 
+  GMSServices.provideAPIKey("AIzaSyDt86GfCXb45aYlvJT_iMxE7me2bKSCFbk");
+}
+
 @NgModule({
   imports: [
     NativeScriptModule,
